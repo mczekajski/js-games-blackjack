54a29c2da2d7a68c0f44cfb153683c233629baa7
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-01-15 19:16:46 +0100

    Add Table class

--- app/Game.js
+++ app/Game.js
@@ -1,13 +1,12 @@
 import { Deck } from "./Deck.js";
 import { Player } from "./Player.js";
+import { Table } from "./Table.js";
 
 class Game {
-  constructor({player, playersCards, dealersCards}) {
+  constructor({player, table}) {
       this.player = player;
       this.dealer = new Player('Krupier');
-
-      this.playersCards = playersCards;
-      this.dealersCards = dealersCards;
+      this.table = table;
       this.deck = new Deck();
       this.deck.shuffle();
   }
@@ -20,20 +19,20 @@ class Game {
       for (let n = 0; n < 2; n++) {
         let card1 = this.deck.pickOne();
         this.player.hand.addCard(card1);
-        this.playersCards.appendChild(card1.render());
+        this.table.showPlayersCard(card1.render());
 
         let card2 = this.deck.pickOne();
         this.dealer.hand.addCard(card2);
-        this.dealersCards.appendChild(card2.render());
+        this.table.showDealersCard(card2.render());
       }
   }
 }
 
+const table = new Table(document.getElementById('playersCards'), document.getElementById('dealersCards'))
 const player = new Player('Marcin');
 const game = new Game({
-    player: player,
-    playersCards: document.getElementById('playersCards'),
-    dealersCards: document.getElementById('dealersCards')
+    player,
+    table
 });
 
 game.run();
