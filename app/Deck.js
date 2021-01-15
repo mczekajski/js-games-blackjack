6b6a825dfbebb143467028468e90505d4415a373
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-01-15 13:14:46 +0100

    Add Player and Hand classes

--- app/Deck.js
+++ app/Deck.js
@@ -10,13 +10,12 @@ export class Deck {
   }
 
   shuffle() {
-    for (let i = this.cards.length - 1; i > 0; i--) {
+    for (let i = 0; i < this.cards.length; i++) {
       const j = Math.floor(Math.random() * i);
       const temp = this.cards[i];
       this.cards[i] = this.cards[j];
       this.cards[j] = temp;
     }
-
     return this.cards;
   }
 }
--- app/Game.js
+++ app/Game.js
@@ -1,4 +1,22 @@
-import { Deck } from './Deck.js';
+import { Deck } from "./Deck.js";
+import { Player } from "./Player.js";
 
-const deck = new Deck();
-console.log(deck.shuffle());
+class Game {
+  constructor(playersCard, dealersCards, player) {
+      this.player = player;
+      this.playersCard = playersCard;
+      this.dealersCards = dealersCards;
+  }
+
+  run() {
+    const deck = new Deck();
+    console.log(deck.shuffle());
+  }
+}
+
+const player = new Player('Marcin');
+const game = new Game({
+    player,
+    playersCards: document.getElementById('playersCards'),
+    dealersCards: document.getElementById('dealersCards')
+});
